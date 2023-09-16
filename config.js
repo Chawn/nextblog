cd17f67764cf865a0d1619347288e3916a86675d
chawn <chawput@gmail.com>
2023-09-16 23:02:10 +0700

    28: cloudinary

--- config.js
+++ config.js
@@ -2,8 +2,12 @@ DB_URI =
 	'mongodb+srv://chawput:7cuiBk1X9Szq3bre@nextblog.g1hniwm.mongodb.net/?retryWrites=true&w=majority';
 API = 'http://localhost:3000/api';
 NEXTAUTH_SECRET = 'asdfasdfasdfasdfgfdsg';
-GOOGLE_CLIENT_ID = '897701599276-9vj7i533g7r1cd74r728rj7ise4bl2d3.apps.googleusercontent.com';
+GOOGLE_CLIENT_ID =
+	'897701599276-9vj7i533g7r1cd74r728rj7ise4bl2d3.apps.googleusercontent.com';
 GOOGLE_CLIENT_SECRET = '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';
+
+CLOUDINARY_UPLOAD_PRESET = 'tfwwv1nh';
+CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dkfqjb4yh/nextblog/uploads';
 
 module.exports = {
 	DB_URI,
@@ -11,4 +15,6 @@ module.exports = {
 	NEXTAUTH_SECRET,
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET,
+	CLOUDINARY_UPLOAD_PRESET,
+	CLOUDINARY_URL,
 };
